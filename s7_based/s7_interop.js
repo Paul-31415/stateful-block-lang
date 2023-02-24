7e0c09f03c641b870ab38b345282091305d9dbb3
Paul Soulanille <paul@soulanille.net>
2023-02-24 10:59:31 -0800

    add better foreign object/function support, local 'this' in scheme

--- s7_based/s7_interop.js
+++ s7_based/s7_interop.js
@@ -2,32 +2,137 @@
 if (Module.s7_interop === undefined){
     //Module.s7_interop.fn($0,$1).apply(Module.s7_interop.list($0,$2));
     //char* typ = s7_object_to_c_string(sc,s7_type_of(sc,obj));
-    let s7_type_str = Module.cwrap("s7_type_str", "ptr", ["ptr","ptr"])
-    let s7_object_to_c_string = Module.cwrap("s7_object_to_c_string", "ptr", ["ptr","ptr"])
-    let s7_car = Module.cwrap("s7_car", "ptr", ["ptr"])
-    let s7_cdr = Module.cwrap("s7_cdr", "ptr", ["ptr"])
-    let s7_boolean = Module.cwrap("s7_boolean", "boolean", ["ptr","ptr"])
-    let s7_symbol_name = Module.cwrap("s7_symbol_name", "ptr", ["ptr"])
-    let s7_string = Module.cwrap("s7_string", "ptr", ["ptr"])
-    let s7_real = Module.cwrap("s7_real", "double", ["ptr"])
-    let s7_real_part = Module.cwrap("s7_real_part", "double", ["ptr"])
-    let s7_imag_part = Module.cwrap("s7_imag_part", "double", ["ptr"])
+    let s7 = {
+	type_str : Module.cwrap("s7_type_str", "ptr", ["ptr","ptr"]),
+	object_to_c_string : Module.cwrap("s7_object_to_c_string", "ptr", ["ptr","ptr"]),
+	car : Module.cwrap("s7_car", "ptr", ["ptr"]),
+	cdr : Module.cwrap("s7_cdr", "ptr", ["ptr"]),
+	set_car : Module.cwrap("s7_set_car", "ptr", ["ptr","ptr"]),
+	set_cdr : Module.cwrap("s7_set_cdr", "ptr", ["ptr","ptr"]),
+	boolean : Module.cwrap("s7_boolean", "boolean", ["ptr","ptr"]),
+	symbol_name : Module.cwrap("s7_symbol_name", "ptr", ["ptr"]),
+	string : Module.cwrap("s7_string", "ptr", ["ptr"]),
+	real : Module.cwrap("s7_real", "double", ["ptr"]),
+	real_part : Module.cwrap("s7_real_part", "double", ["ptr"]),
+	imag_part : Module.cwrap("s7_imag_part", "double", ["ptr"]),
+    
+    
+	gc_protect : Module.cwrap("s7_gc_protect", "ptr", ["ptr","ptr"]),
+	gc_unprotect_at : Module.cwrap("s7_gc_unprotect_at", "none", ["ptr","ptr"]),
+	gc_protected_at : Module.cwrap("s7_gc_protected_at", "ptr", ["ptr","ptr"]),
+	gc_protect_via_stack : Module.cwrap("s7_gc_protect_via_stack", "ptr", ["ptr","ptr"]),
+	gc_unprotect_via_stack : Module.cwrap("s7_gc_unprotect_via_stack", "ptr", ["ptr","ptr"]),
+	gc_protect_via_location : Module.cwrap("s7_gc_protect_via_location", "ptr", ["ptr","ptr","ptr"]),
+	gc_unprotect_via_location : Module.cwrap("s7_gc_unprotect_via_location", "ptr", ["ptr","ptr"]),
+	gc_on : Module.cwrap("s7_gc_on", "ptr", ["ptr","boolean"]),
+
+
+	
+	f : Module.cwrap("s7_f","ptr",["ptr"]),
+	t : Module.cwrap("s7_t","ptr",["ptr"]),
+	nil : Module.cwrap("s7_nil","ptr",["ptr"]),
+	undefined_ : Module.cwrap("s7_undefined","ptr",["ptr"]),
+	unspecified : Module.cwrap("s7_unspecified","ptr",["ptr"]),
+	eof_object : Module.cwrap("s7_eof_object","ptr",["ptr"]),
+
+	make_string : Module.cwrap("s7_make_string","ptr",["ptr","string"]),
+	make_real : Module.cwrap("s7_make_real","ptr",["ptr","double"]),
+	make_complex : Module.cwrap("s7_make_complex","ptr",["ptr","double","double"]),
+	cons : Module.cwrap("s7_cons","ptr",["ptr","ptr","ptr"]),
+	make_c_pointer : Module.cwrap("s7_make_c_pointer","ptr",["ptr","ptr"]),
+	make_c_pointer_with_type : Module.cwrap("s7_make_c_pointer_with_type","ptr",["ptr","ptr","ptr","ptr"]),
+	make_symbol : Module.cwrap("s7_make_symbol","ptr",["ptr","string"]),
+	symbol_value : Module.cwrap("s7_symbol_value","ptr",["ptr","ptr"]),
+	symbol_set_value : Module.cwrap("s7_symbol_set_value","ptr",["ptr","ptr","ptr"]),
+	symbol_local_value : Module.cwrap("s7_symbol_local_value","ptr",["ptr","ptr","ptr"]),
+	
+	
+	make_js_ref : Module.cwrap("make_js_ref","ptr",["ptr","ptr"]),
+
+	call_with_location : Module.cwrap("s7_call_with_location","ptr",["ptr","ptr","ptr","string","string","ptr"]),
+	eval_with_location : Module.cwrap("s7_eval_with_location","ptr",["ptr","ptr","ptr","string","string","ptr"]),
+
+	define_variable_with_documentation : Module.cwrap("s7_define_variable_with_documentation","ptr",["ptr","string","ptr","string"]),
+	define : Module.cwrap("s7_define","ptr",["ptr","ptr","ptr","ptr"]),
+	closure_let : Module.cwrap("s7_closure_let","ptr",["ptr","ptr"]),
+    }
+    let clear_stdout_stderr = Module.cwrap("clear_stdout_stderr","none",[]);
     let eval_string = Module.cwrap("eval_string", "string", ["string"]);
+    let get_out = Module.cwrap("get_out", "string", []);
+    let get_err = Module.cwrap("get_err", "string", []);
+    let eval_string_to_obj = Module.cwrap("eval_string_to_obj","ptr",["string"]);
+    let gc_is_on = {};
+    function gc_off(sc){
+	if (!(sc in gc_is_on)){
+	    gc_is_on[sc] = true;
+	}
+	let r = gc_is_on[sc];
+	gc_is_on[sc] = false;
+	s7.gc_on(sc,false)
+	return r;
+    }
+    function gc_on(sc,on=true){
+	if (on){
+	    gc_is_on[sc] = true;
+	    s7.gc_on(sc,true)
+	}
+    }
+    
+    function log_outputs(){
+	let out = get_out();
+	let err = get_err();
+	clear_stdout_stderr();
+	if (out) console.log(out);
+	if (err) console.warn(err);
+    }
+    function log_outputs_throw(e,sc,o){
+	let out = get_out();
+	let err = get_err();
+	clear_stdout_stderr();
+	if (out) console.log(out);
+	if (err) {
+	    if (e === undefined){
+		throw new Error(err);
+	    }else{
+		e.message = err;
+		e.name = (sc!==undefined && o!==undefined)?repr(sc,o):"Error"; 
+		throw e;
+	    }
+	}
+    }
+    function eval_s(code){
+	let result = eval_string(code);
+	log_outputs();
+	return result;
+    }
+
+    function evs_p(code){
+	let r = eval_string_to_obj(code)
+	log_outputs();
+	return r;
+    }
+    function evs(code){
+	let s = sc();
+	let r = evs_p(code);
+	return to_js(s,r);
+    }
+    
+    
     function string(obj){
-	return Module.UTF8ToString(s7_string(obj));
+	return Module.UTF8ToString(s7.string(obj));
     }
     function symbol_name(obj){
-	return Module.UTF8ToString(s7_symbol_name(obj));
+	return Module.UTF8ToString(s7.symbol_name(obj));
     }
     
     function repr(sc,obj){
-	let s = s7_object_to_c_string(sc,obj);
+	let s = s7.object_to_c_string(sc,obj);
 	let r = Module.UTF8ToString(s);
 	Module._free(s);
 	return r;
     }
     function type_of(sc,obj){
-	let s = s7_type_str(sc,obj);
+	let s = s7.type_str(sc,obj);
 	let r = Module.UTF8ToString(s);
 	Module._free(s);
 	return r;
@@ -47,9 +152,9 @@ if (Module.s7_interop === undefined){
 	let t = type_of(sc,obj);
 	let seen = {}
 	while (t === "pair?"){
-	    r.push(to_js(sc,s7_car(obj)));
+	    r.push(to_js(sc,s7.car(obj)));
 	    seen[obj] = true;
-	    obj = s7_cdr(obj);
+	    obj = s7.cdr(obj);
 	    if (seen[obj] === true){
 		throw new Error("s7_interop.list was passed an infinite list: "+repr(sc,obj));
 	    }
@@ -70,8 +175,8 @@ if (Module.s7_interop === undefined){
 	}
     }
     function rational(s){
-	let n,d = s.split("/");
-	return new Rational(BigInt(n),BigInt(d));
+	s = s.split("/");
+	return new Rational(BigInt(s[0]),BigInt(s[1]));
     }
     class Complex{
 	constructor(r,i){
@@ -85,23 +190,127 @@ if (Module.s7_interop === undefined){
     function complex(r,i){
 	return new Complex(r,i);
     }
+    const jsObjects = {};
+    function lookupJsObject(sc,id){
+	return jsObjects[sc] && jsObjects[sc][id];
+    }
+    function registerJsObject(sc,id,obj){
+	if (jsObjects[sc] === undefined){
+	    jsObjects[sc] = {}
+	}
+	jsObjects[sc][id] = obj;
+    }
+    function freeJsObject(sc,id){
+	if (jsObjects[sc] !== undefined){
+	    delete jsObjects[sc][id];
+	}
+    }
+    
+    const s7objects = {};
+    const s7objectRegistry = new FinalizationRegistry((v) => {
+	s7.gc_unprotect_at(v.sc,v.loc);
+	if (s7objects[v.sc]){
+	    delete s7objects[v.sc][v.addr];
+	}
+	//console.log("gc",v);
+    });
+    function conslike(obj){
+	return ("car" in obj) && ("cdr" in obj);
+    }
+    function is_integer(v){
+	return (typeof v) === "bigint" || ((typeof v) === "number" && Number.isInteger(v));
+    }
+    function ratiolike(obj){
+	return ("n" in obj) && ("d" in obj) && is_integer(obj.n) && is_integer(obj.d);
+    }
+    function complexlike(obj){
+	return ("r" in obj) && ("i" in obj) && (typeof obj.r) === "number" && (typeof obj.i) === "number";
+    }
+    function scobjlike(obj){
+	return ("sc" in obj) && ("addr" in obj);
+    }
+    function protectS7Object(obj,sc,addr){
+	let loc = s7.gc_protect(sc,addr);
+	s7objectRegistry.register(obj,{sc,addr,loc},obj);
+	if (s7objects[sc] === undefined){
+            s7objects[sc] = {};
+        }
+	s7objects[sc][addr] = new WeakRef(obj);
+	return loc;
+    }
     class Pair{
 	constructor(sc,obj){
-	    this.sc = sc
-	    this.addr = obj
+	    this.sc = sc;
+	    this.addr = obj;
+	    this.protect();
+	}
+	protect(){
+	    this.loc = protectS7Object(this,this.sc,this.addr);
+	}
+	free(){
+	    if (this.loc !== undefined){
+		s7objectRegistry.unregister(this);
+		s7.gc_unprotect_at(this.sc,this.loc);
+		this.loc = undefined;
+	    }
 	}
 	get car(){
-	    return to_js(this.sc,s7_car(this.addr));
+	    return to_js(this.sc,s7.car(this.addr));
 	}
 	get cdr(){
-	    return to_js(this.sc,s7_cdr(this.addr));
+	    return to_js(this.sc,s7.cdr(this.addr));
+	}
+	toString(seen){
+	    return repr(this.sc,this.addr);
 	}
     }
     function pair(sc,obj){
-	return new Pair(sc,obj);
+	return (s7objects[sc] && s7objects[sc][obj] && s7objects[sc][obj].deref()) || new Pair(sc,obj);
     }
     let symbolTable = {};
     let unspecified = Symbol("unspecified");
+    let eof_object = Symbol("eof");
+    class UnknownType{
+	constructor(sc,addr,t){
+	    this.sc = sc;
+	    this.addr = addr;
+	    this.type = t;
+	}
+	toString(){
+	    return repr(this.sc,this.addr);
+	}
+    }
+    function schemeFunc(sc,obj){
+	let f = (s7objects[sc] && s7objects[sc][obj] && s7objects[sc][obj].deref());
+	if (f){
+	    return f;
+	}
+	let e = new Error();
+	f = function (...args){
+	    let alist = null;
+	    for (let i = args.length-1 ;i >= 0; i--){
+		alist = {car:args[i],cdr:alist};
+	    }
+	    //console.log(alist);
+	    
+	    //this encloser
+	    s7.define(sc,s7.closure_let(sc,obj),s7.make_symbol(sc,"this"),to_sc(sc,this));
+	    
+	    //s7.symbol_set_value(s7, s7.make_symbol(s7, "this"), to_sc(sc,this));
+	    //s7.eval_with_location(sc, s7.cons(s7.make_symbol(s7, "define") ,s7.cons(s7.make_symbol(s7, "this"),s7.cons(to_sc(sc,this),s7.nil(sc)))),
+	    //s7.nil(sc),		  
+	    //s7.symbol_set_value(s7, s7.make_symbol(s7, "this"), to_sc(sc,this))
+	    
+	    let r = s7.call_with_location(sc,obj,to_sc(sc,alist),"javascript", "run", 0);
+	    log_outputs_throw(e,sc,r);
+	    return to_js(sc,r);
+	}
+	f.sc = sc;
+	f.addr = obj;
+	f.loc = protectS7Object(f,sc,obj);
+	f.error = e;
+	return f;
+    }
     function to_js(sc, obj){
 	let t = type_of(sc,obj);
 	//console.log(t,repr(sc,obj));
@@ -111,9 +320,9 @@ if (Module.s7_interop === undefined){
 	case "rational?":
 	    return rational(repr(sc,obj));
 	case "float?":
-	    return s7_real(obj);
+	    return s7.real(obj);
 	case "complex?":
-	    return complex(s7_real_part(obj),s7_imag_part(obj));
+	    return complex(s7.real_part(obj),s7.imag_part(obj));
 	case "string?":
 	    return string(obj);
 	case "symbol?":
@@ -127,14 +336,113 @@ if (Module.s7_interop === undefined){
 	    return undefined;
 	case "unspecified?":
 	    return unspecified;
+	case "eof-object?":
+	    return eof_object;
 	case "pair?":
 	    return pair(sc,obj);
+	case "procedure?":
+	    return schemeFunc(sc,obj);
+	case "js-ref?":
+	    return lookupJsObject(sc,obj);
+	case "vector?":
+	case "hash-table?":
+	case "character?":
+	case "c-pointer?":
 	default:
-	    return {sc:sc,addr:obj,type:t};
+	    return new UnknownType(sc,obj,t);
 	}
-	return [scheme_ptr, obj_ptr];
     }
+    function with_gc_off(sc,func,args){
+	let g = gc_off(sc);
+	let r = func.apply(null,args);
+	s7.gc_protect_via_stack(sc,r);
+	gc_on(sc,g);
+	return r;
+    }
+    function to_sc(sc,obj,seen){
+	return with_gc_off(sc,_to_sc,[sc,obj,seen]);
+    }
+    function to_s7(obj){
+	return to_sc(sc(),obj);
+    }
+    let js_functions = [];
+    function _to_sc(sc,obj,seen){
+	if (seen === undefined){
+	    seen = new Map();
+	}
+	if (seen.has(obj)){
+	    return seen.get(obj);
+	}
+	let r;
+	switch(typeof obj){
+	case "object":
+	    if (obj === null){
+		return s7.nil(sc);
+	    }else if (scobjlike(obj)){
+		return obj.addr;
+	    }else if (conslike(obj)){
+		let nil = s7.nil(sc);
+		r = s7.cons(sc,nil,nil);
+		seen.set(obj,r);
+		s7.set_car(r,_to_sc(sc,obj.car,seen));
+		s7.set_cdr(r,_to_sc(sc,obj.cdr,seen));
+		return r;
+	    }else if (ratiolike(obj)){
+		let n = obj.n; let d = obj.d;
+		if (d.toString().charAt(0) === "-"){
+		    n = -n; d = -d;
+		}
+		r = evs_p(n+"/"+d);
+		seen.set(obj,r);
+		return r;
+	    }else if (complexlike(obj)){
+		r = s7.make_complex(sc,obj.r,obj.i);
+		seen.set(obj,r);
+		return r;
+	    }else{
+		//throw new Error("unsupported object");
+		//r = s7.make_c_pointer(sc,)
+		r = s7.make_js_ref(sc,s7.nil(sc));
+		registerJsObject(sc,r,obj);
+		seen.set(obj,r);
+		return r;
+	    }
+	case "undefined":
+	    return s7.undefined_(sc);
+	case "boolean":
+	    return obj?s7.t(sc):s7.f(sc);
+	case "number":
+	    r = s7.make_real(sc,obj);
+	    seen.set(obj,r);return r; 
+	case "bigint":
+	    r = evs_p(sc,obj.toString());
+	    seen.set(obj,r);return r; 
+	case "string":
+	    r = s7.make_string(sc,obj);
+	    seen.set(obj,r);return r; 
+	case "symbol":
+	    if (obj === unspecified){
+		return s7.unspecified(sc);
+	    }else if (obj === eof_object){
+		return s7.eof_object(sc);
+	    }else{
+		let name = Symbol.keyFor(obj);
+		r = s7.make_symbol(sc,name)
+		symbolTable[r] = obj;
+		seen.set(obj,r);return r; 
+	    }
+	case "function":
+	    let i = js_functions.length;
+	    js_functions.push(obj);
+	    let f = "(lambda x (apply call-js-p (cons \"Module.s7_interop.js_functions["+i+"]\" x)))";
+	    r = evs_p(f);
+	    seen.set(obj,r);return r; 
+	}
 
+	return seen.get(obj);
+    }
+    
+    
     let sc_cache = undefined;
     function sc(){
 	if (sc_cache === undefined){
@@ -145,20 +453,39 @@ if (Module.s7_interop === undefined){
 	
     
     Module.s7_interop = {
-	s7_type_str,
-	s7_object_to_c_string,
-	s7_car,s7_cdr,s7_boolean, s7_symbol_name, s7_string, s7_real, s7_real_part, s7_imag_part,
+	s7,
+	
 	string, symbol_name,
 	repr,
 	type_of,
 	fn,
 	list,
 	to_js,
-	eval_string,
+	to_sc,to_s7,
+	eval_string,get_out,get_err,eval_string_to_obj, clear_stdout_stderr,log_outputs,log_outputs_throw,
+	eval_s,evs_p,evs,
 
+	pair,Pair,conslike,
+	s7objectRegistry,
+	s7objects,
+
+	schemeFunc,
+	
+	rational,Rational,ratiolike,
+	complex,Complex,complexlike,
+	UnknownType,scobjlike,
+	
 	sc,
 	symbolTable,
 	unspecified,
+	eof_object,
+
+	gc_off,gc_on,
+
+
+	js_functions,
+
+	jsObjects, lookupJsObject,registerJsObject,freeJsObject,
     };
 }
 
--- s7_based/s7_wasm.c
+++ s7_based/s7_wasm.c
@@ -42,9 +42,35 @@ const char* get_err() {
 }
 
 
+const s7_pointer eval_string_to_obj(char* str) {
+     s7_close_output_port(g_sc, g_out);
+     s7_close_output_port(g_sc, g_err);
 
+     // stdout
+     g_out = s7_open_output_string(g_sc);
+     s7_set_current_output_port(g_sc, g_out);
 
+     // stderr
+     g_err = s7_open_output_string(g_sc);
+     s7_set_current_error_port(g_sc, g_err);
 
+     s7_pointer result = s7_eval_c_string(g_sc, str);
+     return result;
+}
+  
+
+const void clear_stdout_stderr() {
+     s7_close_output_port(g_sc, g_out);
+     s7_close_output_port(g_sc, g_err);
+
+     // stdout
+     g_out = s7_open_output_string(g_sc);
+     s7_set_current_output_port(g_sc, g_out);
+
+     // stderr
+     g_err = s7_open_output_string(g_sc);
+     s7_set_current_error_port(g_sc, g_err);
+}
 /*
   note for s7.h@164
   young objects being gc-protected could be replaced with a construct
@@ -93,7 +119,7 @@ static s7_pointer console_log(s7_scheme * sc, s7_pointer args){
 }
 
 
-const char* s7_type_str(s7_scheme * sc, s7_pointer obj){ return s7_object_to_c_string(sc,s7_type_of(sc,obj));}
+
 //const bool s7_bool(s7_scheme * sc, s7_pointer arg){ return s7_boolean(sc,arg);}
 
 /*
@@ -190,6 +216,152 @@ static s7_pointer call_js_p(s7_scheme * sc, s7_pointer args){
   return rv;
 }
 
+
+static s7_pointer eval_js(s7_scheme * sc, s7_pointer args){
+  // arglist: (fn_or_string . fnargs)
+  s7_pointer rv = EM_ASM_PTR({
+      return Module.s7_interop.to_sc($0,Module.s7_interop.fn($0,$1));
+    },(void*) sc ,(void*) s7_car(args));
+  return rv;
+}
+
+
+
+
+
+
+
+/*typedef struct {
+  s7_pointer index;
+  } js_ref;*/
+
+static int js_ref_type_tag = 0;
+
+static s7_pointer js_ref_to_string(s7_scheme *sc, s7_pointer args)
+{
+  //js_ref *o = (js_ref *)s7_c_object_value(s7_car(args));  
+  s7_pointer result = EM_ASM_PTR({
+      return Module.s7_interop.to_sc($0,'<js '+Module.s7_interop.lookupJsObject($0,$1)+'>');
+    },sc,s7_car(args));// o->index);
+  return(result);
+}
+
+static s7_pointer free_js_ref(s7_scheme *sc, s7_pointer obj)
+{
+  EM_ASM({
+      Module.s7_interop.freeJsObject($0,$1);
+    },sc,obj);//((js_ref*)obj)->index);
+  //free(s7_c_object_value(obj));
+  return(NULL);
+}
+static s7_pointer mark_js_ref(s7_scheme *sc, s7_pointer obj)
+{
+  return (NULL);
+  //js_ref *o = (js_ref *)s7_c_object_value(obj);
+  //s7_mark(o->data);
+}
+const s7_pointer make_js_ref(s7_scheme *sc, s7_pointer args)
+{
+  s7_pointer r = s7_make_c_object(sc, js_ref_type_tag, 0);
+  if (!s7_is_null(sc,args)){
+    EM_ASM({
+	Module.s7_interop.registerJsObject($0,$2,Module.s7_interop.to_js($0,$1));
+      },(void*) sc ,(void*) s7_car(args),r);
+  }else{
+    EM_ASM({
+	Module.s7_interop.registerJsObject($0,$1,undefined);
+      },(void*) sc ,r);
+  }
+  return r;
+}
+static s7_pointer is_js_ref(s7_scheme *sc, s7_pointer args)
+{
+  return(s7_make_boolean(sc, 
+			 s7_is_c_object(s7_car(args)) &&
+			 s7_c_object_type(s7_car(args)) == js_ref_type_tag));
+}
+static s7_pointer js_ref_typeof(s7_scheme *sc, s7_pointer args)
+{
+  return EM_ASM_PTR({
+      return Module.s7_interop.to_sc($0,typeof Module.s7_interop.lookupJsObject($0,$1));
+    },sc,s7_car(args));
+}
+
+
+static s7_pointer js_ref_data(s7_scheme *sc, s7_pointer args)
+{
+  return EM_ASM_PTR({
+      return Module.s7_interop.to_sc($0,Module.s7_interop.lookupJsObject($0,$1));
+    },sc,s7_car(args));
+}
+static s7_pointer set_js_ref_data(s7_scheme *sc, s7_pointer args)
+{
+  EM_ASM({
+      Module.s7_interop.registerJsObject($0,$1,Module.s7_interop.to_js($0,$2));
+    },sc,s7_car(args),s7_cadr(args));
+  return s7_car(args);
+}
+
+
+static s7_pointer js_ref_member(s7_scheme *sc, s7_pointer args)
+{
+  return EM_ASM_PTR({
+      return Module.s7_interop.to_sc($0,Module.s7_interop.lookupJsObject($0,$1)[Module.s7_interop.to_js($0,$2)]);
+    },sc,s7_car(args),s7_cadr(args));
+}
+static s7_pointer set_js_ref_member(s7_scheme *sc, s7_pointer args)
+{
+  EM_ASM({
+      Module.s7_interop.lookupJsObject($0,$1)[Module.s7_interop.to_js($0,$2)] = Module.s7_interop.to_js($0,$3);
+    },sc,s7_car(args),s7_cadr(args),s7_caddr(args));
+  return s7_car(args);
+}
+static s7_pointer js_ref_method(s7_scheme *sc, s7_pointer args)
+{
+  return EM_ASM_PTR({
+      let o = Module.s7_interop.lookupJsObject($0,$1);
+      let f = o[Module.s7_interop.to_js($0,$2)];
+      if (typeof f === 'function'){
+	f = f.bind(o);
+      }
+      return Module.s7_interop.to_sc($0,f);
+    },sc,s7_car(args),s7_cadr(args));
+}
+
+
+
+static s7_pointer js_ref_is_equal(s7_scheme *sc, s7_pointer args) 
+{
+  s7_pointer p1 = s7_car(args);
+  s7_pointer p2 = s7_cadr(args);
+  if (p1 == p2) 
+    return(s7_t(sc));
+  if ((!s7_is_c_object(p2)) ||
+      (s7_c_object_type(p2) != js_ref_type_tag))
+    return(s7_f(sc));
+  int r = EM_ASM_INT({
+      return Module.s7_interop.lookupJsObject($0,$1)==Module.s7_interop.lookupJsObject($0,$2) ? 1:0;
+    },sc,p1,p2);
+  return(s7_make_boolean(sc,r != 0));
+}
+
+
+
+static s7_pointer type_of(s7_scheme *sc, s7_pointer arg)
+{
+  if (s7_is_c_object(arg)){
+    if (s7_c_object_type(arg) == js_ref_type_tag){
+      return s7_name_to_value(sc,"js-ref?");
+    }
+  }
+  return s7_type_of(sc,arg);
+}
+static s7_pointer s_type_of(s7_scheme *sc, s7_pointer args){
+  return type_of(sc,s7_car(args));
+}
+const char* s7_type_str(s7_scheme * sc, s7_pointer obj){ return s7_object_to_c_string(sc,type_of(sc,obj));}
+
+
 int main() {
      s7_scheme *s7 = g_sc = s7_init();
      g_out = g_err = s7_nil(g_sc);
@@ -197,9 +369,33 @@ int main() {
      s7_define_function(s7, "saddr", saddr, 0, 0, false, "(saddr) returns the memory address of the scheme");
      s7_define_function(s7, "console.log", console_log, 1, 0, true, "(console.log thing) prints and returns thing");
      s7_define_function(s7, "call-js", call_js, 1, 0, true, "(call-js thing . args) calls thing(..args), returns (thing . args)");
-     s7_define_function(s7, "call-js-i", call_js_i, 1, 0, true, "(call-js thing . args) calls thing(..args), returns an int32");
-     s7_define_function(s7, "call-js-d", call_js_d, 1, 0, true, "(call-js thing . args) calls thing(..args), returns a double");
-     s7_define_function(s7, "call-js-p", call_js_p, 1, 0, true, "(call-js thing . args) calls thing(..args), returns a scheme object");
+     s7_define_function(s7, "call-js-i", call_js_i, 1, 0, true, "(call-js-i thing . args) calls thing(..args), returns an int32");
+     s7_define_function(s7, "call-js-d", call_js_d, 1, 0, true, "(call-js-d thing . args) calls thing(..args), returns a double");
+     s7_define_function(s7, "call-js-p", call_js_p, 1, 0, true, "(call-js-p thing . args) calls thing(..args), returns a scheme object");
+     s7_define_function(s7, "eval-js", eval_js, 1, 0, false, "(eval-js thing) does eval(thing), returns a scheme object");
+
+     
+     
+     js_ref_type_tag = s7_make_c_type(s7, "js-ref");
+     s7_c_type_set_gc_free(s7, js_ref_type_tag, free_js_ref);
+     s7_c_type_set_gc_mark(s7, js_ref_type_tag, mark_js_ref);
+     s7_c_type_set_is_equal(s7, js_ref_type_tag, js_ref_is_equal);
+     s7_c_type_set_to_string(s7, js_ref_type_tag, js_ref_to_string);
+     
+     s7_define_function(s7, "make-js-ref", make_js_ref, 0, 1, false, "(make-js-ref x) makes a new javascript object equal to to_js(x), with no arguments, makes an undefined");
+     s7_define_function(s7, "js-ref?", is_js_ref, 1, 0, false, "(js-ref? anything) returns #t if its argument is a javascript object");
+     s7_define_function(s7, "js-ref-typeof", js_ref_typeof, 1, 0, false, "(js-ref-typeof x) returns typeof x");
+     s7_define_variable(s7, "js-ref-data", 
+                     s7_dilambda(s7, "js-ref-data", js_ref_data, 1, 0, set_js_ref_data, 2, 0, "js-ref data field."));
+     s7_define_variable(s7, "js-ref-member", 
+			s7_dilambda(s7, "js-ref-member", js_ref_member, 2, 0, set_js_ref_member, 3, 0, "(js-ref-member x y) is x[y]"));
+     s7_define_variable(s7, "js-ref-method", 
+			s7_dilambda(s7, "js-ref-method", js_ref_method, 2, 0, set_js_ref_member, 3, 0, "(js-ref-method x y) is x[y].bind(x)"));
+     
+
+
+     //extend to support custom types;
+     s7_define_function(s7, "type-of",s_type_of,1,0,false,"(type-of obj) returns a symbol describing obj's type");
      return 0;
 }
 
