3dcf6de014f3f1f713478841fde9084ea9893eb5
Paul Soulanille <paul@soulanille.net>
2023-05-18 18:12:24 -0700

    change eval to eval-string

--- s7_based/import.scm
+++ s7_based/import.scm
@@ -1,7 +1,7 @@
 (provide 'import.scm)
 (define fetch (eval-js 'fetch))
 (define (load-remote path)
-  (eval (append "(begin\n" (await-js (fetch path)) "\n)"))
+  (eval-string (append "(begin\n" (await-js (fetch path)) "\n)"))
   )
 (define-macro (require file)
   `(unless (provided? ',file)
@@ -10,5 +10,5 @@
 
 (unless (provided? 'await.scm);bootstrap await.scm because await is needed to do nice requires
   (write "using fallback require for await.scm . determinisim hasn't been tested here!")
-  ((eval-js "(ev)=>fetch('await.scm').then((r)=>(ev('(begin\\n'+r.text()+'\\n)')))") eval)
+  ((eval-js "(ev)=>fetch('await.scm').then((r)=>(ev('(begin\\n'+r.text()+'\\n)')))") eval-string)
   )
